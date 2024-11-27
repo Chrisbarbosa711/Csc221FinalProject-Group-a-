471134437f04d4f7c329d79ba5e67de0be447f8e
Chrisbarbosa711 <chrisbarbosa711@gmail.com>
2024-11-27 11:58:44 -0500

    Create User.java

---
+++ GUI/User.java
@@ -0,0 +1,44 @@
+package GUI;
+import java.util.ArrayList;
+
+public class User{
+
+  private String name;
+  private ArrayList<String> workouts = new ArrayList<String>();
+
+  public void setName(String name){
+    this.name = name;
+  }
+  public String returnName(){
+    return this.name;
+  }
+
+  public void addWorkout(String workout){
+    this.workouts.add(workout);
+  }
+
+  public void removeWorkout(String workout){
+    this.workouts.remove(workout);
+  }
+
+  public void returnWorkouts(){
+    System.out.printf("Workouts added:\n");
+    if(this.workouts.size() == 0){
+      System.out.println("You have no workouts");
+    }
+    else{
+      for(String w : workouts){
+        System.out.println(w);
+      }
+    }
+  }
+
+  public Boolean checkWorkout(String workout){
+    if(workouts.size() == 0){
+      return false;
+    }
+    return this.workouts.contains(workout);
+  }
+  
+  
+}
