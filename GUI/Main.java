d582db3b3dd85e85389c9362d14ae3b8fae7a0dd
m <malmakt000@citymail.cuny.edu>
2024-12-09 12:06:37 -0500

    Update Main.java

--- GUI/Main.java
+++ GUI/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		Windows instance = new Windows();
-	    instance.myWindows();
+	    	instance.myWindows();
+
+		Load create = new Load();
+		create.run();
 
 	}
 
