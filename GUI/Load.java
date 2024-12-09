d167d8df3952aea4189a08dc04cadc3b79bf4bf8
m <malmakt000@citymail.cuny.edu>
2024-12-09 12:05:19 -0500

    Update Load.java

--- GUI/Load.java
+++ GUI/Load.java
@@ -39,20 +39,20 @@ public class Load {
 		
 	public void run(){
 		JFrame frame = new JFrame("Select Day");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setBackground(Color.PINK);
-        frame.setSize(1000, 1000); 
-        
-    	JPanel Buttons = new JPanel();
-    	Buttons.setLayout(new GridLayout(1, 7, 20, 0));
-    	
-    	JPanel Pages = new JPanel(new CardLayout());
-    	
-    	String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-    	for (String day: week) {
-            DayButton dayButton = new DayButton(Buttons, Pages, day);
-            Buttons.add(dayButton);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setLayout(new BorderLayout());
+	        frame.setBackground(Color.PINK);
+	        frame.setSize(1000, 1000); 
+	        
+	    	JPanel Buttons = new JPanel();
+	    	Buttons.setLayout(new GridLayout(1, 7, 20, 0));
+	    	
+	    	JPanel Pages = new JPanel(new CardLayout());
+	    	
+	    	String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+	    	for (String day: week) {
+	            DayButton dayButton = new DayButton(Buttons, Pages, day);
+	            Buttons.add(dayButton);
         }
 
          frame.add(Buttons, BorderLayout.NORTH); 
