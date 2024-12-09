4b7f81b1257e81af3e53d38b25e0cf239613d494
m <malmakt000@citymail.cuny.edu>
2024-12-09 12:03:24 -0500

    Update Load.java

--- GUI/Load.java
+++ GUI/Load.java
@@ -4,12 +4,40 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Load {
-	public static void main(String[] args) {
+	public class DayButton extends JButton {
+		
+		DayButton(JPanel panel, JPanel day, String text){
+			super(text);
+			panel.add(this);
+			
+			JPanel dayPage = new JPanel();
+			dayPage.setBackground(Color.PINK);
+			dayPage.setLayout(new BorderLayout());
+			
+			JLabel dayLabel = new JLabel(text, JLabel.CENTER);
+			dayPage.add(dayLabel, BorderLayout.CENTER);
+			day.add(dayPage, text);
+			
+			this.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent event) {
+					CardLayout layout = (CardLayout) day.getLayout();
+	                layout.show(day, text);
+				}
+			});
+		}
+	}
+		
+	public void run(){
 		JFrame frame = new JFrame("Select Day");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
