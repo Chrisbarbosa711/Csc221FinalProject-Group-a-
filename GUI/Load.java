e0b749f6a190c9b2714454c5c902e9f2d0a81a32
kritanbaniya <120137661+kritanbaniya@users.noreply.github.com>
2024-12-11 04:15:43 -0500

    refresh log

--- GUI/Load.java
+++ GUI/Load.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class Load extends JPanel {
 
     public void refreshDayExercises(String day, JPanel Pages) {
+    	System.out.println("Refreshing " + day);
         ResultSet exercises = DBconnection.dayExercises(day);
         try {
             ArrayList<String[]> exerciseList = UnpackRS.unpackDayExercise(exercises);
