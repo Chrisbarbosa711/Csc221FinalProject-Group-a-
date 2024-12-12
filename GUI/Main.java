e948032c97ea28a18016aa095e0e5923b11fbbdb
Chrisbarbosa711 <chrisbarbosa711@gmail.com>
2024-12-12 13:18:07 -0500

    added comments

--- GUI/Main.java
+++ GUI/Main.java
@@ -5,8 +5,9 @@ import SQLtest.DBconnection;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
+		//we define a connection to the database using the SQLtest package provided and we declare an
+		//object of the windows class in order to run the application
 		DBconnection.establishConnection();
 		Windows instance = new Windows();
 	    	instance.myWindows();
