8a91846d7be734c3214616bc394e0f8ab8947b85
m <malmakt000@citymail.cuny.edu>
2024-12-12 17:04:50 -0500

    Update Windows.java

--- GUI/Windows.java
+++ GUI/Windows.java
@@ -1,5 +1,5 @@
 package GUI;
-import SQLtest.DBconnection; //so that we can use the database connection functionality 
+import SQLtest.DBconnection;
 
 import javax.swing.*;
 import java.awt.event.*;
@@ -10,49 +10,79 @@ public class Windows extends JFrame{
   
     public void myWindows(){
     	//define an instance of the user class so we can use the info entered at launch-screen
-        final User instance = new User();
-    
+    	final User instance = new User();
+
         //we create our launch window which serves are the basis of the application
         //along with essential labels and formatting effects
         final JFrame launchPage = new JFrame("Fitness application");
-        JLabel LPGreeting = new JLabel("Welcome to the fitness tracker application", SwingConstants.CENTER);
-        JLabel LogInLabel = new JLabel("please enter your name in the textbox below:", SwingConstants.CENTER);
+        launchPage.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 10, 5, 10);
+        
+        JLabel LPGreeting = new JLabel("Welcome to the fitness tracker application");
+        JLabel LogInLabel = new JLabel("please enter your name in the textbox below:");
         LPGreeting.setFont(new Font("Arial", Font.PLAIN, 30));
 
         //button for submitting the login name
         final JButton submitButton = new JButton("Submit");
+        
         //appears when the entered text for login is valid 
         JButton EnterApp = new JButton("Enter");
-        //we initially set this button visibility as false so that it only shows up when we want it to
         EnterApp.setVisible(false);
+        //we initially set this button visibility as false so that it only shows up when we want it to
         final JTextField textField = new JTextField(20);
-
+        
         //add all the buttons,etc. to the window and call .pack() which makes it so everything that we set are at
-        //there correct sizes and positions in the window
-        launchPage.add(LPGreeting, BorderLayout.NORTH);
-        launchPage.add(textField);
-        launchPage.add(submitButton);
-        launchPage.add(EnterApp, BorderLayout.SOUTH);
-        launchPage.add(LogInLabel, BorderLayout.CENTER);
+        //there correct sizes and positions in the window 
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1; 
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        gbc.fill = GridBagConstraints.BOTH;
+        launchPage.add(LPGreeting, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        launchPage.add(LogInLabel, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        launchPage.add(textField, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 1;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.EAST;
+        launchPage.add(submitButton, gbc);
+        
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.WEST;
+        launchPage.add(EnterApp, gbc);
         launchPage.pack();      
     
         // Set the size of the frame
-        launchPage.setSize(475, 500);
+        launchPage.setSize(600, 600);
+        launchPage.setLocationRelativeTo(null);
 
-        
         EnterApp.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	//we wrap the rest of the application functionality into a JPanel
             	//provided by the Load class, so once we enter from the launch screen, we remove everything
             	//that was previously visible
-                Load loadPanel = new Load();
-                // Remove specific components
-                launchPage.remove(LPGreeting);
-                launchPage.remove(LogInLabel);
-                launchPage.remove(textField);
-                launchPage.remove(submitButton);
-                launchPage.remove(EnterApp);
+            	Load loadPanel = new Load();
                 
+            	//remove previous stuff
+                launchPage.getContentPane().removeAll();
+                
+                //loadPanel is in a different layout so we have to change theJFrame layout
+                launchPage.setLayout(new BorderLayout());
                 launchPage.add(loadPanel, BorderLayout.CENTER);
                 loadPanel.run();
                 launchPage.revalidate();
@@ -60,7 +90,7 @@ public class Windows extends JFrame{
             }
         });
 
-        submitButton.setBounds(800, 420, 100, 50);
+        
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -71,10 +101,10 @@ public class Windows extends JFrame{
                 	//shows up as a pane window that the user has to close in order to continue entering text
                 	JOptionPane.showMessageDialog(null, "Please enter a valid username");
                 }
-                //if the input is valid then we make the enter button visible for them to enter the application
                 else {
+                	//if the input is valid then we make the enter button visible for them to enter the application
                 	submitButton.setVisible(false);
-                      EnterApp.setVisible(true);
+                    EnterApp.setVisible(true);
                 }
             }
         });
@@ -98,7 +128,7 @@ public class Windows extends JFrame{
         launchPage.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                DBconnection.closeConnection(); 
+                DBconnection.closeConnection();
                 System.out.println("Database connection closed.");
             }
         });
