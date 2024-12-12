d1ed8fea78fa2cbed56b2c8e3f9f4b83ca71504c
Chrisbarbosa711 <chrisbarbosa711@gmail.com>
2024-12-12 13:15:45 -0500

    added comments

--- GUI/User.java
+++ GUI/User.java
@@ -1,6 +1,10 @@
 package GUI;
 import java.util.ArrayList;
 
+
+//this class serves as a storage for the user information that the person running the program enters in the launch screen
+//currently the usage is somewhat limited but would allow us the utilize this information for account creation, etc.
+//functionality that would require more time that alloted but are entirely possible with the utilization for a user class
 public class User {
 	
 	private String name;
